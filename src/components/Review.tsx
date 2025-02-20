e8f4b40457d41d4168213fad09fd93a7beea3000
Mae Ibarra <maeann013@gmail.com>
2025-02-20 14:10:05 +0400

    updated

--- src/components/Review.tsx
+++ src/components/Review.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Review = () => {
   return (
-    <section className="relative w-full flex items-center justify-center gap-6 p-8">
+    <section className="relative text-gray-700 w-full flex items-center justify-center gap-6 p-8">
       <div className="text-center relative">
         <div className="text-sm">⭐ ⭐ ⭐ ⭐ ⭐ </div>
         <div className="text-lg font-semibold">
